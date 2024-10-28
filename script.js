e6c8919bf433c00a0812f535c2a24f85c25c2b40
Ovomaltine <tanguybis1@gmail.com>
2024-10-28 16:07:25 +0100

    add files

---
+++ script.js
@@ -0,0 +1,13 @@
+    document.getElementById('myForm').addEventListener('submit', async function (event) {
+        event.preventDefault();
+        const formData = new FormData(this);
+        const data = Object.fromEntries(formData.entries());
+
+        const response = await fetch('/.netlify/functions/handleForm', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        });
+        const result = await response.json();
+        console.log(result.message); // Affiche un message de confirmation
+    });

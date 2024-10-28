b456fba6e49be8d99d74b490c2f5b7d672d37720
Ovomaltine <tanguybis1@gmail.com>
2024-10-28 16:21:33 +0100

    redirect to netflix

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-    document.getElementById('loginForm').addEventListener('submit', async function (event) {
-        event.preventDefault();
-        const formData = new FormData(this);
-        const data = Object.fromEntries(formData.entries());
-
-        const response = await fetch('/.netlify/functions/handleForm', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data),
-        });
-        const result = await response.json();
-        console.log(result.message); // Affiche un message de confirmation
-    });
+function redirect() {
+    window.location.href = "https://www.netflix.com";
+}
